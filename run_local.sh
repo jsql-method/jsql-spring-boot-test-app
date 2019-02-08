2af6bec9cceaf091c73092983b03cce625890744
Adam Radecki <adam.radecki@jsql.it>
2019-02-08 09:43:13 +0000

    Update run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,3 @@
 gradle bootJar
 sleep 1
-java -jar ./build/libs/jsql-spring-boot-test-app.jar --spring.datasource.url=jdbc:postgresql://localhost:5432/plugins_test?ssl=false --spring.datasource.username=postgres --spring.datasource.password=pgMjk6F#18P,5
+java -jar ./build/libs/jsql-spring-boot-test-app.jar --spring.datasource.url=jdbc:postgresql://172.32.1.31:5452/plugins_test?ssl=false --spring.datasource.username=postgres_user --spring.datasource.password=fgfdbc45trdgf
