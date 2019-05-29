90c1ddf04ffb0ff85a42b6a838a7bb6e9651ff4e
Łukasz Kołyga <lukasz.kolyga@gmail.com>
2019-05-29 20:14:51 +0200

    fix

--- src/main/java/pl/jsql/controller/TestController.java
+++ src/main/java/pl/jsql/controller/TestController.java
@@ -1,5 +1,7 @@
 package pl.jsql.controller;
 
+import it.jsql.connector.controller.JSQLController;
+import it.jsql.connector.dto.JSQLConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,4 +20,8 @@ public class TestController extends JSQLController {
         return new JSQLService(applicationContext, "==XxPiAgZ3bAX6ZbbcvcbT6wqrPdJqQDXi+mBTK/zZTw==L818RZMBZ1mnOnOoSdMZ", "4qjHbugc2M4+L4KNCeDhOA==");
     }
 
+    @Override
+    public JSQLConfig getConfig() {
+        return null;
+    }
 }
