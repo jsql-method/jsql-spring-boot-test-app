bc0e04ea6940c5f349a620782b98959cf8deb43a
Dawid Senko <dawid.senko@jsql.pl>
2019-05-29 21:33:59 +0200

    +fix

--- src/main/java/pl/jsql/controller/TestController.java
+++ src/main/java/pl/jsql/controller/TestController.java
@@ -7,21 +7,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @CrossOrigin
 @RestController
 public class TestController extends JSQLController {
 
-    @Autowired
-    ApplicationContext applicationContext;
-
-    @Override
-    public IJSQLService getJsqlService() {
-        return new JSQLService(applicationContext, "==XxPiAgZ3bAX6ZbbcvcbT6wqrPdJqQDXi+mBTK/zZTw==L818RZMBZ1mnOnOoSdMZ", "4qjHbugc2M4+L4KNCeDhOA==");
-    }
-
     @Override
     public JSQLConfig getConfig() {
-        return null;
+        return new JSQLConfig("dawid.senko@jsql.it", "dawid.senko@jsql.it");
     }
+
 }
